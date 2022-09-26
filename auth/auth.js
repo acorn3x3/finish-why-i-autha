c7899c4a2f8d68c15056efe0bb9acb59331a1a21
Andrew Cornejo <atcornejo3@gmail.com>
2022-09-26 14:16:58 -0700

    creating dev branch

--- auth/auth.js
+++ auth/auth.js
@@ -73,7 +73,9 @@ authForm.addEventListener('submit', async (e) => {
     //      - get formData object from form
     //      - call "authType.action" passing in the email and password from
     //        the form data and assign to response variable
-
+    const formData = new FormData(authForm);
+    const response = await authType.action(formData.get('email'), formData.get('password'));
+    // 2 lines above new
     const error = response.error;
 
     if (error) {
@@ -83,6 +85,7 @@ authForm.addEventListener('submit', async (e) => {
         authButton.textContent = buttonText;
     } else {
         // go back to wherever user came from
+        location.replace(redirectUrl);
         // > Part A using "location", replace url with "redirectUrl"
     }
 });
